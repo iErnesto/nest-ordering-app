002512b0a83765da661ca788482981ca3b7cbb08
Ernesto Hernandez <ernesto1991diaz@gmail.com>
2022-11-11 10:56:26 -0600

    Config mongodb and docker

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,24 @@
+import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from '../schemas/order.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
