9aae8e65d21ad793a157b0525701a194531c95c6
Ernesto Hernandez <ernesto1991diaz@gmail.com>
2022-11-15 08:29:53 -0600

    Create jwt guard feature

---
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,54 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AUTH_SERVICE } from './services';
+import { ClientProxy } from '@nestjs/microservices';
+import { catchError, Observable, tap } from 'rxjs';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(@Inject(AUTH_SERVICE) private authClient: ClientProxy) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const authentication = this.getAuthentication(context);
+    return this.authClient
+      .send(`validate_user`, {
+        Authentication: authentication,
+      })
+      .pipe(
+        tap((res) => {
+          this.addUser(res, context);
+        }),
+        catchError(() => {
+          throw new UnauthorizedException();
+        }),
+      );
+  }
+  private getAuthentication(context: ExecutionContext) {
+    let authentication: string;
+    if (context.getType() === 'rpc') {
+      authentication = context.switchToRpc().getData().Authentication;
+    } else if (context.getType() === 'http') {
+      authentication = context.switchToHttp().getRequest()
+        .cookies?.Authentication;
+    }
+    if (!authentication) {
+      throw new UnauthorizedException(
+        'No value was provided for authentication',
+      );
+    }
+    return authentication;
+  }
+  private addUser(user: any, context: ExecutionContext) {
+    if ((context.switchToRpc().getData().user = user)) {
+    } else if (context.getType() === 'http') {
+      context.switchToHttp().getRequest().user = user;
+    }
+  }
+}
